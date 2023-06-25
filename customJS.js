5b32c72bd492bc51306a64094bfd031eb0c93511
Adil <mdadil.shahrukh@mindtickle.com>
2023-06-25 10:41:57 +0530

    broken the code into smaller files

---
+++ customJS.js
@@ -0,0 +1,259 @@
+const config = window.factorConfig;
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+  $(".selectpicker").selectpicker("mobile");
+}
+
+const getSummaryItemHTML = ({ description, label, value, id }) => `
+              <li id="summary${id}" class="list-group-item d-flex justify-content-between lh-condensed">
+                <div>
+                  <h6 class="my-0">${label}</h6>
+                  ${
+                    description
+                      ? `<small class='text-muted'>${description}</small>`
+                      : ""
+                  }
+                </div>
+                <span class="text-muted" id="summaryVal${id}">${value}</span>
+              </li>`;
+
+const getItemHTML = ({ id, min, max, label, step, value }) => `
+      <div class="form-group mb-4" id="container${id}">
+        <label for="${id}"
+          >${label}<span class="text-muted">(min: ${min}, max: ${max})</span>
+          <span data-toggle="tooltip" title="Remove this factor" data-placement="bottom" class="remove-feature" onclick="removeFeature(${id})">⛔️</span>
+        </label>
+        <div class="row">
+          <div class="col-9 d-flex">
+            <input
+              id="${id}"
+              min="${min}"
+              max="${max}"
+              type="range"
+              step="${step}"
+              value="${value}"
+              class="form-control-range factors-input"
+              oninput="handleRangeChange(this);" />
+          </div>
+
+          <div class="col-3 d-flex">
+            <input
+              min="${min}"
+              max="${max}"
+              type="number"
+              step="${step}"
+              value="${value}"
+              required
+              id="${id}_current"
+              class="form-control factors-input factors-input-box"
+              oninput="validity.valid||handleRangeInput(this, ${id});" />
+          </div>
+        </div>
+      </div>`;
+
+const showSWSError = ({ message }) => {
+  const errorMsgEl = document.querySelector(
+    "label.text-danger.small.font-weight-bold.error-helper"
+  );
+  errorMsgEl.innerText = message;
+  document.querySelector(".selectpicker-wrp").classList.add("error-show");
+};
+
+const removeSWSError = () => {
+  document.querySelector(".selectpicker-wrp").classList.remove("error-show");
+};
+
+const addNewFactor = (event) => {
+  var selectedItemValue = $(".selectpicker").val();
+  console.log(selectedItemValue);
+  if (!selectedItemValue) {
+    showSWSError({ message: "Please select a value from the dropdown" });
+  } else {
+    removeSWSError();
+
+    const selectedConfigItemKey = Object.keys(config).find(
+      (configItemKey) => config[configItemKey].label === selectedItemValue
+    );
+
+    if (
+      document.querySelectorAll(`#container${selectedConfigItemKey}`).length
+    ) {
+      showSWSError({
+        message: "This value already exists",
+      });
+      return;
+    }
+
+    const formHTMLToBeInserted = getItemHTML({
+      id: selectedConfigItemKey,
+      ...config[selectedConfigItemKey],
+    });
+    const summaryHTMLToBeInserted = getSummaryItemHTML({
+      id: selectedConfigItemKey,
+      ...config[selectedConfigItemKey],
+    });
+
+    const topLevelElement = document.getElementById("swsTopEl");
+    $(formHTMLToBeInserted).insertAfter(topLevelElement);
+
+    const summaryULContainer = document.getElementById("listOfRisks");
+    $(summaryHTMLToBeInserted).insertBefore(summaryULContainer);
+    updateCountOnFactorsChange();
+  }
+};
+
+const handleRangeChange = (props) => {
+  const currentInputElement = document.querySelector(`#${props.id}_current`);
+  currentInputElement.value = props.value;
+
+  const summaryULContainer = document.getElementById(`summaryVal${props.id}`);
+
+  summaryULContainer.innerText = props.value;
+};
+
+$(document).ready(function () {
+  console.log("ready!");
+  console.log($("#swsFactors"));
+  // <option data-subtext="Two">Two</option>
+
+  $('[data-toggle="tooltip"]').tooltip({ container: "body" });
+
+  window.CSS.registerProperty({
+    name: "--percentage",
+    syntax: "<number>",
+    inherits: true,
+    initialValue: 0,
+  });
+
+  const wrapperElement = new DocumentFragment();
+
+  for (item of Object.values(config)) {
+    const optionElement = document.createElement("option");
+    optionElement.innerText = item.label;
+    wrapperElement.append(optionElement);
+  }
+  $("#swsFactors").append(wrapperElement);
+});
+
+var validateAndGetFormData = () => {
+  var genderEl = document.querySelector('[name="gender"]:checked');
+  var ageEl = document.querySelector("#Age");
+  var factorsInputArr = document.querySelectorAll(
+    "input.form-control-range.factors-input"
+  );
+
+  if (!genderEl || !genderEl.value) {
+    throw new Error("Pls input gender");
+  }
+  if (!ageEl || !ageEl.value) {
+    throw new Error("Pls input age");
+  }
+
+  if (factorsInputArr.length < 3) {
+    throw new Error("Pls select at least 3 factors to continue");
+  }
+
+  // Gender=Male&Age=22&num_features=3&Feature1=Anxiety&Score_Feature1=3
+  var formData = new FormData();
+  formData.append("Gender", genderEl.value);
+  formData.append("Age", ageEl.value);
+  formData.append("num_features", factorsInputArr.length);
+
+  factorsInputArr.forEach((item, index) => {
+    formData.append(`Feature${index + 1}`, item.id);
+    formData.append(`Score_Feature${index + 1}`, item.value);
+  });
+
+  return formData;
+};
+
+var showLoadingOnSubmitCTA = (element, onClick) => {
+  const loaderModal = document.getElementById("fullPageLoader");
+  const submitFormCTAEl = document.getElementById("submitFormCTA");
+  loaderModal.classList.add("d-flex");
+  submitFormCTAEl.innerText = "Predicting using some AI Magic";
+  submitFormCTAEl.classList.add("disabled");
+};
+
+$("#pdpForm").bind("submit", async function (e) {
+  e.preventDefault();
+
+  try {
+    var formData = validateAndGetFormData();
+    showLoadingOnSubmitCTA();
+    let response = await fetch("/submit", {
+      method: "POST",
+      body: formData,
+    });
+    const result = await response.json();
+    console.log(result, "PENDING PASS SCENARIO HANDLING");
+    var dd = result.confidenceScore;
+    var PP = result.userStatus;
+    document.getElementById("resultProgressbar").style = `--value: ${dd}`;
+    document.getElementById("resultText").innerText = PP;
+    $("#resultModal").modal();
+  } catch (error) {
+    resetSubmitButton();
+    alert(error);
+  }
+});
+
+const resetSubmitButton = () => {
+  const loaderModal = document.getElementById("fullPageLoader");
+  loaderModal.classList.remove("d-flex");
+
+  const submitFormCTAEl = document.getElementById("submitFormCTA");
+  submitFormCTAEl.innerText = "Calculate my risk profile";
+  submitFormCTAEl.classList.remove("disabled");
+};
+
+// Handle modal close
+$("#resultModal").on("hide.bs.modal", resetSubmitButton);
+
+const updateCountOnFactorsChange = () => {
+  const factorsInputArr = document.querySelectorAll(
+    "input.form-control-range.factors-input"
+  );
+
+  const factorsCounterEl = document.querySelector(
+    "span.badge.badge-secondary.badge-pill.factors-count"
+  );
+
+  factorsCounterEl.innerText = factorsInputArr.length;
+};
+
+const handleRangeInput = (event, { id }) => {
+  if (event.stopPropagation) {
+    event.stopPropagation();
+    event.preventDefault();
+  } else if (window.event) {
+    window.event.cancelBubble = true;
+  }
+
+  // const { min, max } = config[id];
+
+  const rangeSliderEl = document.getElementById(id);
+  rangeSliderEl.value = event.value;
+
+  const summaryULContainer = document.getElementById(`summaryVal${id}`);
+  summaryULContainer.innerText = event.value;
+};
+
+$("form").keypress(function (e) {
+  //Enter key
+  if (e.which == 13) {
+    return false;
+  }
+});
+
+const removeFeature = (element) => {
+  /* eslint-disable no-debugger, no-console */
+  const containerId = `container${element.id}`;
+  const summaryId = `summary${element.id}`;
+  console.log("id", containerId);
+  const itemToRemove = document.getElementById(containerId);
+  const summaryItemToRemove = document.getElementById(summaryId);
+  itemToRemove.remove();
+  summaryItemToRemove.remove();
+  updateCountOnFactorsChange();
+};
